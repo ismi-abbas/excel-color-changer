3eae35d197fede8478e5c2cb4d750d7130cd1dcb
Muhammad Abbas <mabbas.mohamadpuzi@petronas.com>
2023-10-09 11:54:59 +0800

    Change color excel

---
+++ index.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+const ExcelJS = require('exceljs');
+
+// Function to process an XLSX file
+async function processXLSXFile(filePath) {
+	const workbook = new ExcelJS.Workbook();
+	await workbook.xlsx.readFile(filePath);
+	const newImageFilePath = path.join(__dirname, 'POTF.png');
+
+	// Loop through each worksheet in the XLSX file
+	workbook.eachSheet(worksheet => {
+		// Loop through all rows and columns in the worksheet
+		worksheet.eachRow(row => {
+			row.eachCell(cell => {
+				if (cell.fill && cell.fill.type === 'pattern' && cell.fill.fgColor && cell.fill.fgColor.argb === 'FF00B3BC') {
+					// Change the background color to #20419A
+					cell.fill = {
+						type: 'pattern',
+						pattern: 'solid',
+						fgColor: { argb: 'FF20419A' }, // Set the new color here
+					};
+				}
+			});
+		});
+	});
+
+	// Save the modified workbook
+	await workbook.xlsx.writeFile(filePath);
+}
+
+// Get a list of all XLSX files in the current directory
+const currentDir = __dirname;
+fs.readdirSync(currentDir).forEach(file => {
+	const filePath = path.join(currentDir, file);
+
+	// Check if the file is an XLSX file
+	if (path.extname(filePath) === '.xlsx') {
+		processXLSXFile(filePath)
+			.then(() => {
+				console.log(`Processed and saved ${filePath}`);
+			})
+			.catch(error => {
+				console.error(`Error processing ${filePath}: ${error.message}`);
+			});
+	}
+});
